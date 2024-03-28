9156a1d31f1eb25a1a51a79b07300ca33eb32f41
luzhibin <1255863672@qq.com>
2024-03-28 22:57:09 +0800

    fix bug

--- src/main/java/com/sqx/modules/posts/dao/PostsDao.java
+++ src/main/java/com/sqx/modules/posts/dao/PostsDao.java
@@ -2,6 +2,8 @@ package com.sqx.modules.posts.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.sqx.modules.posts.entity.PostsEntity;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface PostsDao extends BaseMapper<PostsEntity> {
 }
